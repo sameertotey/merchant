082537b4b8a20cdcf4ed8f7879e89fa52636e5c6
Sameer Totey <sameertotey@hotmail.com>
2014-04-28 11:04:10 -0600

    wrap-up initial setup of the merchant project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
+
 
   root 'products#index'
+  resources :products
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+products = Product.create([{title: "Green Grapes (1 bunnch)", price: 2.00, 
+  description: "1 bunch of approximately 80 green grapes", image_url: "green_grapes.jpg"  },
+  {title: "Purple Grapes (1 bunch)", price: 2.25,
+   description: "1 bunch of approximately 90 purple grapes", image_url: "purple_grapes.jpg"}])
