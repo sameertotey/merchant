adfbf8727a89fe0b6c6cb0403dade980158f35fd
Sameer Totey <sameertotey@hotmail.com>
2014-04-28 09:28:05 -0600

    remove sqllite and use pg instead

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140428134051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: true do |t|
     t.string   "title"
     t.decimal  "price",       precision: 8, scale: 2
